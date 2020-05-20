9e4691b460c2831d40f7a913aea119b4a8e06856
Bill Newman <william.newman@seattlecolleges.edu>
2020-05-20 08:03:17 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,28 @@ var data = require('./data/test.json');//our test data
 var app = express();
 const PORT = 3000;
 
-app.get('/',(req,res) =>{
-  //res.send(`We're at the root of the app, on port ${PORT}`);
-  res.json(data);
-});
+app.use(express.static(__dirname + '/public')); // set location for static files
+app.use(require("body-parser").urlencoded({extended: true})); // parse form submissions
+
+/*
 
 //allows access to the root for serving static files
 app.use(express.static(__dirname));
+
+//access asset files in public folder
+app.use(express.static('public'));
+
+//allows access to images folder
+app.use('/images',express.static('images'));
+
+*/
+
+app.get('/',(req,res) =>{
+  //res.send(`We're at the root of the app, on port ${PORT}`);
+  //res.json(data);
+  res.type('text/html');
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 app.listen(PORT, () =>{
   console.log(`App is listening on port ${PORT}`);
